0399905314df6f9365b23daadf0e233fe11bccf1
Makena-Wambui <iammakena@gmail.com>
2024-08-29 16:40:41 +0300

    Styling container for netflix logo and sign in button

--- flickzone/src/components/header/styles/header.js
+++ flickzone/src/components/header/styles/header.js
@@ -5,7 +5,21 @@ export const Background = styled.div``;
 
 export const Frame = styled.div``;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	display: flex;
+	margin: 0 56px;
+	height: 100px;
+	justify-content: space-between;
+	align-items: center;
+
+	a {
+	display: flex;
+	}
+
+	@media (max-width: 1000px) {
+	margin: 0 30px;
+	}
+`;
 
 export const Logo = styled.img`
 	height: 32px;
