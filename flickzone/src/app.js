819e8d9b4a5e244a91c98afdec9beaec9e8bf8ff
Makena-Wambui <iammakena@gmail.com>
2024-08-29 13:23:55 +0300

    Setting up routes: /browse, /signin, /, /signup, using react-router-dom

--- flickzone/src/app.js
+++ flickzone/src/app.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
+import * as ROUTES from './constants/routes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp } from './pages';
 
 export default function App() {
   return (
-	  <>
-	  <JumbotronContainer />
-	  <FaqsContainer />
-	  <FooterContainer />
-	  </>
+    <Router>
+      <Routes>
+        <Route exact path='/browse' element={<Browse />} />
+        <Route exact path='/signin' element={<SignIn />} />
+        <Route exact path='/signup' element={<SignUp />} />
+        <Route exact path={ROUTES.HOME} element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
---
+++ flickzone/src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { JumbotronContainer } from '../containers/jumbotron';
+import { FooterContainer } from '../containers/footer';
+import { FaqsContainer } from '../containers/faqs';
+
+
+
+export default function Home() {
+  return (
+    <>
+	  <JumbotronContainer />
+	  <FaqsContainer />
+	  <FooterContainer />
+    </>
+  );
+}
