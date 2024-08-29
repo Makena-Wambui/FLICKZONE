185c1083de2c5615a858ce2b9b440606b6f3abea
Makena-Wambui <iammakena@gmail.com>
2024-08-29 17:08:56 +0300

    Moved opt-form bar from accordion to the header component.

--- flickzone/src/pages/home.js
+++ flickzone/src/pages/home.js
@@ -3,17 +3,24 @@ import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
 import { FaqsContainer } from '../containers/faqs';
-
+import { OptForm } from '../components';
 
 
 export default function Home() {
   return (
     <>
 	  <HeaderContainer>
-	  	<JumbotronContainer />
-	  	<FaqsContainer />
-	  	<FooterContainer />
+	  	<OptForm>
+	  		<OptForm.Input placeholder="Email address" />
+	  		<OptForm.Button>Try it now</OptForm.Button>
+	  		<OptForm.Break />
+	  		<OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+	  	</OptForm>
 	  </HeaderContainer>
+
+	  <JumbotronContainer />
+	  <FaqsContainer />
+	  <FooterContainer />
     </>
   );
 }
