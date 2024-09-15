0a58bb1894e2d115bd0ee2cee77191c0a4d7042f
Makena-Wambui <iammakena@gmail.com>
2024-09-15 14:53:08 +0300

    Documented Footer

--- flickzone/src/components/footer/index.js
+++ flickzone/src/components/footer/index.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Container, Row, Column, Link, Title, Text, Break } from './styles/footer';
 
+// Footer component that renders the footer container.
 export default function Footer({ children, ... restProps }) {
 	return <Container { ... restProps}>{children}</Container>;
 };
 
+// The Row component.
 Footer.Row = function FooterRow({ children, ... restProps }) {
 	return <Row {... restProps}>{children}</Row>;
 };
 
+// The Column component.
 Footer.Column = function FooterColumn({ children, ... restProps }) {
         return <Column {... restProps}>{children}</Column>;                                                                               };
 
-// Links to different Pages
+// Links to different Pages in the footer.
 Footer.Link = function FooterLink({ children, ... restProps }) {
         return <Link {... restProps}>{children}</Link>;                                                                                   };
 
+// The Title component.
 Footer.Title = function FooterTitle({ children, ... restProps }) {
         return <Title {... restProps}>{children}</Title>;                                                                                
 };
 
+// The Text component.
 Footer.Text = function FooterText({ children, ... restProps }) {
         return <Text {... restProps}>{children}</Text>;                                                                                   };
 
+// The Break component.
 Footer.Break = function FooterBreak({ children, ... restProps }) {
         return <Break {... restProps}>{children}</Break>;
 };
--- flickzone/src/components/footer/styles/footer.js
+++ flickzone/src/components/footer/styles/footer.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+// The Container component is a styled div element with a display of flex, a padding of 70px on the top and bottom, 56px on the left and right, a margin of auto, a max-width of 1000px, and a flex-direction of column.
 export const Container = styled.div`
 	display: flex;
 	padding: 70px 56px;
@@ -12,7 +13,7 @@ export const Container = styled.div`
 	}
 `;
 
-
+// The Column component is a styled div element with a display of flex and a flex-direction of column.
 export const Column = styled.div`
 	display: flex;
 	flex-direction: column;
@@ -20,6 +21,7 @@ export const Column = styled.div`
 	
 `;
 
+// The Row component is a styled div element with a display of grid, a grid-template-columns of repeat(auto-fill, minmax(230px, 1fr)), and a grid-gap of 15px.
 export const Row = styled.div`
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
@@ -30,6 +32,7 @@ export const Row = styled.div`
         }
 `;
 
+// The Link component is a styled a element with a color of #757575, a margin-bottom of 20px, a font-size of 15px, and a text-decoration of none.
 export const Link = styled.a`
 	color: #757575;
 	margin-bottom: 20px;
@@ -38,18 +41,21 @@ export const Link = styled.a`
 
 `;
 
+// The Title component is a styled p element with a font-size of 16px, a color of #757575, and a margin-bottom of 40px.
 export const Title = styled.p`
 	font-size: 16px;
 	color: #757575;
 	margin-bottom: 40px;
 `;
 
+// The Text component is a styled p element with a font-size of 13px, a color of #757575, and a margin-bottom of 40px.
 export const Text = styled.p`
         font-size: 13px;
         color: #757575;
         margin-bottom: 40px;
 `;
 
+// The Break component is a styled p element with a flex-basis of 100%, a height of 0, and a margin of 0.
 export const Break = styled.p`
         flex-basis: 100%;
 	height: 0;
