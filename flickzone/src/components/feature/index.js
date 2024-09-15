4382e950ec90213b5acfdecca063be8367dea4dc
Makena-Wambui <iammakena@gmail.com>
2024-09-15 14:46:37 +0300

    Documented feature

--- flickzone/src/components/feature/index.js
+++ flickzone/src/components/feature/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import { Title, SubTitle, Container } from './styles/feature';
 
+// Feature component that renders the feature container
 export default function Feature({ children, ... restProps }) {
 	return <Container {... restProps}>{children}</Container>
 }
 
+// Feature Title (Movie Title)
 Feature.Title = function FeatureTitle({ children, ...restProps }) {
 	return <Title {...restProps}>{children}</Title>;
 };
 
+// Feature SubTitle (Genre)
 Feature.SubTitle = function FeatureSubTitle({ children, ...restProps }) {
 	return <SubTitle {...restProps}>{children}</SubTitle>;
 };
--- flickzone/src/components/feature/styles/feature.js
+++ flickzone/src/components/feature/styles/feature.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+// The Container component is a styled div element with a display of flex, a flex-direction of column, a border-bottom of 8px solid #222, text-align of center, and padding of 165px on the top and bottom and 45px on the left and right sides.
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -8,6 +9,7 @@ export const Container = styled.div`
   padding: 165px 45px;
 `;
 
+// The Title component is a styled h1 element with a color of white, a max-width of 640px, a font-size of 50px, a font-weight of 500, and a margin of auto.
 export const Title = styled.h1`
   color: white;
   max-width: 640px;
@@ -20,6 +22,7 @@ export const Title = styled.h1`
   }
 `;
 
+// The SubTitle component is a styled h2 element with a color of white, a font-size of 26px, a font-weight of normal, and a margin of 16px on the top and bottom and auto on the left and right sides.
 export const SubTitle = styled.h2`
   color: white;
   font-size: 26px;
