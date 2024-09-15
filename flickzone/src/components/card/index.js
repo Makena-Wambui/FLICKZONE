8832b7d99ed4ea2d55f31f6264dd72b1bddbf4e4
Makena-Wambui <iammakena@gmail.com>
2024-09-15 14:41:44 +0300

    Document Card component

--- flickzone/src/components/card/index.js
+++ flickzone/src/components/card/index.js
@@ -18,8 +18,10 @@ import {
   Image,
 } from './styles/card';
 
+// Feature Context for the card
 export const FeatureContext = createContext();
 
+//Card component (Main component) that renders the card container
 export default function Card({ children, ...restProps }) {
   const [showFeature, setShowFeature] = useState(false);
   const [itemFeature, setItemFeature] = useState({});
@@ -31,33 +33,40 @@ export default function Card({ children, ...restProps }) {
   );
 }
 
+// Card Group (Group of cards) 
 Card.Group = function CardGroup({ children, ...restProps }) {
   return <Group {...restProps}>{children}</Group>;
 };
 
+//Card Title (Movie Title) 
 Card.Title = function CardTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
 };
 
+//Card SubTitle (Genre)
 Card.SubTitle = function CardSubTitle({ children, ...restProps }) {
   return <SubTitle {...restProps}>{children}</SubTitle>;
 };
 
+//Card Text (Description)
 Card.Text = function CardText({ children, ...restProps }) {
   return <Text {...restProps}>{children}</Text>;
 };
 
+//Card Entities (Group of items)
 Card.Entities = function CardEntities({ children, ...restProps }) {
   return <Entities {...restProps}>{children}</Entities>;
 };
 
+// Card Meta (Meta data)
 Card.Meta = function CardMeta({ children, ...restProps }) {
   return <Meta {...restProps}>{children}</Meta>;
 };
 
+//Card Item (Individual item) that renders the item container
 Card.Item = function CardItem({ item, children, ...restProps }) {
   const { setShowFeature, setItemFeature } = useContext(FeatureContext);
-
+  
   return (
     <Item
       onClick={() => {
@@ -71,10 +80,12 @@ Card.Item = function CardItem({ item, children, ...restProps }) {
   );
 };
 
+//Card Image (Image of the item)
 Card.Image = function CardImage({ ...restProps }) {
   return <Image {...restProps} />;
 };
 
+//Card Feature (Feature of the item) that renders the feature container
 Card.Feature = function CardFeature({ children, category, ...restProps }) {
   const { showFeature, itemFeature, setShowFeature } = useContext(FeatureContext);
 
--- flickzone/src/components/card/styles/card.js
+++ flickzone/src/components/card/styles/card.js
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
+// The Title component is a styled p element with a font-size of 24px, a color of #e5e5e5, a font-weight of bold, and a margin of 56px on the left and right sides and 0 on the top. 
 export const Title = styled.p`
-  font-size: 24px;
-  color: #e5e5e5;
+  font-size: 24px; 
+  color: #e5e5e5; 
   font-weight: bold;
   margin-left: 56px;
   margin-right: 56px;
   margin-top: 0;
 `;
 
+// The Container component is a styled div element with a display of flex, a flex-direction of column, and a margin-bottom of 50px.
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -25,6 +27,7 @@ export const Container = styled.div`
   }
 `;
 
+// The Group component is a styled div element with a display of flex, a flex-direction of row or column, an align-items property that is set to the value of the alignItems prop, and a margin property that is set to the value of the margin prop.
 export const Group = styled.div`
   display: flex;
   flex-direction: ${({ flexDirection }) => (flexDirection === 'row' ? 'row' : 'column')};
@@ -38,6 +41,7 @@ export const Group = styled.div`
   }
 `;
 
+// The SubTitle component is a styled p element with a font-size of 12px, a color of #fff, a font-weight of bold, a margin-top of 0, a margin-bottom of 0, and a user-select property set to none.
 export const SubTitle = styled.p`
   font-size: 12px;
   color: #fff;
@@ -48,6 +52,7 @@ export const SubTitle = styled.p`
   display: none;
 `;
 
+// The Text component is a styled p element with a margin-top of 5px, a font-size of 10px, a color of #fff, a margin-bottom of 0, a user-select property set to none, a display property set to none, and a line-height property set to normal.
 export const Text = styled.p`
   margin-top: 5px;
   font-size: 10px;
@@ -58,11 +63,13 @@ export const Text = styled.p`
   line-height: normal;
 `;
 
+// The Entities component is a styled div element with a display of flex and a flex-direction of row.
 export const Entities = styled.div`
   display: flex;
   flex-direction: row;
 `;
 
+// The Meta component is a styled div element with a display of none, a position of absolute, a bottom of 0, a padding of 10px, and a background-color of #0000008f.
 export const Meta = styled.div`
   display: none;
   position: absolute;
@@ -71,6 +78,7 @@ export const Meta = styled.div`
   background-color: #0000008f;
 `;
 
+// The Image component is a styled img element with a border of 0, a width of 100%, a max-width of 305px, a cursor of pointer, a height of auto, and a padding and margin of 0.
 export const Image = styled.img`
   border: 0;
   width: 100%;
@@ -81,6 +89,7 @@ export const Image = styled.img`
   margin: 0;
 `;
 
+// The Item component is a styled div element with a display of flex, a flex-direction of column, a margin-right of 5px, a position of relative, and a cursor of pointer. It also has a transition property that changes the transform property over 0.2 seconds when the item is hovered over. When the item is hovered over, the transform property is set to scale(1.3) and the z-index is set to 99. When the item is hovered over, the Meta, Text, and SubTitle components are displayed with a z-index of 100. The first Item component has a margin-left of 56px, and the last Item component has a margin-right of 56px.
 export const Item = styled.div`
   display: flex;
   flex-direction: column;
@@ -118,6 +127,7 @@ export const Item = styled.div`
   }
 `;
 
+// The FeatureText component is a styled p element with a font-size of 18px, a color of white, a font-weight of bold if the fontWeight prop is set to bold, and a margin of 0. The line-height is set to 22px when the screen width is less than 600px.
 export const FeatureText = styled.p`
   font-size: 18px;
   color: white;
@@ -129,6 +139,7 @@ export const FeatureText = styled.p`
   }
 `;
 
+// The Feature component is a styled div element with a display of flex, a flex-direction of row, a background-image that is set to the value of the src prop, a background-size of contain, a position of relative, a height of 360px, a background-position-x of right, a background-repeat of no-repeat, and a background-color of black. When the screen width is less than 1000px, the height is set to auto, the background-size is set to auto, the font-size of the Title component is set to 20px, the line-height of the Title component is set to 20px, the margin-bottom of the Title component is set to 10px, and the font-size of the FeatureText component is set to 14px.
 export const Feature = styled.div`
   display: flex;
   flex-direction: row;
@@ -155,10 +166,12 @@ export const Feature = styled.div`
   }
 `;
 
+// The FeatureTitle component is a styled Title component with a margin-left of 0.
 export const FeatureTitle = styled(Title)`
   margin-left: 0;
 `;
 
+// The FeatureClose component is a styled button element with a color of white, a position of absolute, a right of 20px, a top of 20px, a cursor of pointer, a background-color of transparent, and a border of 0. The img element inside the FeatureClose component has a filter of brightness(0) invert(1) and a width of 24px.
 export const FeatureClose = styled.button`
   color: white;
   position: absolute;
@@ -174,6 +187,7 @@ export const FeatureClose = styled.button`
   }
 `;
 
+// The Content component is a styled div element with a margin of 56px, a max-width of 500px, and a line-height of normal. When the screen width is less than 1000px, the margin is set to 30px and the max-width is set to none.
 export const Content = styled.div`
   margin: 56px;
   max-width: 500px;
@@ -185,6 +199,7 @@ export const Content = styled.div`
   }
 `;
 
+// The Maturity component is a styled div element with a background-color of #f44336 if the rating is greater than or equal to 15, and #2f9600 if the rating is less than 15. It has a border-radius of 15px, a width of 28px, a line-height of 28px, a text-align of center, a color of white, a font-weight of bold, a text-shadow of 2px 2px 4px rgba(0, 0, 0, 0.2), a margin-right of 10px, and a font-size of 12px.
 export const Maturity = styled.div`
   background-color: ${({ rating }) => (rating >= 15 ? '#f44336' : '#2f9600')};
   border-radius: 15px;
